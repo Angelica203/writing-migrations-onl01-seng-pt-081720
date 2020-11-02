de3aaee05de3b076af9308d6a4b2e952cb798ac9
Angelica203 <brantleyhomebuyer@gmail.com>
2020-11-02 17:01:05 -0500

    Done.

---
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -0,0 +1,10 @@
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.2]
+
+    def change
+      add_column :students do |t|
+        t integer :grade
+        t.string :birthday
+      end
+    end
+
+end
